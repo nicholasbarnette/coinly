9cc98efad2e3c13733e25c37207c334f9ce26fb3
Nick Barnette <nicdavid@live.unc.edu>
2018-05-26 08:46:59 -0400

    Added some inputs.

--- src/index.js
+++ src/index.js
@@ -1,34 +1,73 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import TopNav from './navigation.js';
 
+import TopNav from './navigation.js';
+import User from './user.js';
+
+/*
 var test = [1, 34, 3, 42, 53, 6, 6, 3, 3, 4];
 class Table extends React.Component {
 	
 	createRows = () => {
 		let rows = [];
 		for (var i in test) {
-			  rows.push(<div class="content"><span>{test[i]}</span></div>);
+			  rows.push(<div className="content"><span>{test[i]}</span></div>);
 		}
 		return rows;
 	}
 	
 	render() {
 		return (
-			<div class="container">{this.createRows()}</div>
+			<div className="container">{this.createRows()}</div>
 		);
 	}
 }
 
+*/
+
 class Display extends React.Component {
-  render() {
-    return (
-      <div className="display">
-        <Table />
-      </div>
-    );
-  }
+	
+	user = null;
+	
+	constructor(props) {
+		super(props)
+		this.state = {
+			first: '',
+			last: '',
+			username: '',
+		}
+	}
+	
+	validateUser = () => {
+		let first = this.state.first;		
+		let last = this.state.last;		
+		let username = this.state.username;
+		
+		if (first !== '' && last !== '' && username !== '') {
+			console.log(true);
+			this.user = new User(first, last, username);
+		} else {
+			console.log(false);
+		}
+	}
+	
+	render() {
+		return (
+			<div className="display">
+				<input id="first" type="text" placeholder="First Name" value={this.state.first} onChange={e => this.handleChange('first', e)} />
+				<input id="last" type="text" placeholder="Last Name" value={this.state.last} onChange={e => this.handleChange('last', e)} />
+				<input id="username" type="text" placeholder="Username" value={this.state.username} onChange={e => this.handleChange('username', e)} />
+				<button type="submit" onClick={this.validateUser}>Submit</button>
+			</div>
+		);
+	}
+	
+	handleChange(key, e) {
+		let str = ('{"' + key + '": "' + e.target.value + '"}').toString();
+		let json = JSON.parse(str);
+		this.setState(json);
+	}
 }
 
 // ========================================
--- src/navigation.js
+++ src/navigation.js
@@ -1,24 +1,18 @@
 import React from 'react';
-//import ReactDOM from 'react-dom';
 import './nav.css';
 
 export default class TopNav extends React.Component {
-	username = 'Nick Barnette';
-	getUsername = () => {
-		return this.username;
-	}
-	
 	render() {
 		return (
-			<nav class="navbar">
-				<div class="navGroup left">
-					<div class="navItem logo">
+			<nav className="navbar">
+				<div className="navGroup left">
+					<div className="navItem logo">
 						<a href="/">App Name</a>
 					</div>
 				</div>
-				<div class="navGroup right">
-					<div class="navItem user">
-						<a href="/">{this.getUsername()}</a>
+				<div className="navGroup right">
+					<div className="navItem user">
+						<a href="/">{this.state.user.getFullName()}</a>
 					</div>
 				</div>
 			</nav>
