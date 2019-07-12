842290a814011a50847e043e18e68df775ebc52e
Nick Barnette <nicdavid@live.unc.edu>
2019-07-11 20:13:32 -0400

    Fixing mobile sizing.

--- static/js/components/notification.js
+++ static/js/components/notification.js
@@ -16,7 +16,7 @@ export default class Notification extends React.Component {
 
 	render() {
 		return (
-			<div className="notification" onClick={e => this.handleOpenClick()} style={{width: this.props.notificationOpen ? '3rem' : 'auto', height: this.props.notificationOpen ? '3.5rem' : 'auto', display: this.props.notification == '' ? 'none' : 'flex'}}>
+			<div className="notification" onClick={e => this.handleOpenClick()} style={{width: this.props.notificationOpen ? '3rem' : '100%', height: this.props.notificationOpen ? '3.5rem' : 'auto', display: this.props.notification == '' ? 'none' : 'flex'}}>
                 <span className="notificationIcon"><Glyphicon glyph="warning-sign" /></span>
 				<span className="notificationText">{this.props.notification}</span>
 				<span className="notificationClose" onClick={e => this.handleCloseClick()}><Glyphicon glyph="remove" /></span>
